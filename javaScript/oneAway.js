e32de65774598c0f88c4a1ea4c2905830a2caf32
LWRGitHub <LoganReynoldsLWR@gmail.com>
2022-08-14 14:29:02 -0700

    oneAway JS

--- javaScript/oneAway.js
+++ javaScript/oneAway.js
@@ -10,5 +10,43 @@
 
 
 
-const oneAway = () =>{}
+const addOrDel1 = (lgStr, smStr, [smIdx,lgIdx]=[0,0]) =>{
+    while(lgIdx < lgStr.length){
+        if(lgStr[lgIdx] !== smStr[smIdx]){
+            lgIdx++;
+            if(lgIdx-smIdx > 1) return false;
+        }else{
+            smIdx++;
+            lgIdx++;
+        }
+    }
+    return true;
+}
 
+const swap1 = (s1, s2, [idx,swapC]=[0,0]) =>{
+    while(idx < s1.length){
+        if(s1[idx] !== s2[idx]){
+            swapC++;
+            if(swapC > 1) return false;
+        }
+        idx++;
+    }
+    return true;
+}
+
+const oneAway = (s1, s2) =>{
+    if(s1 === s2) return true;
+    if(s1.length - s2.length === 1){
+        return addOrDel1(s1, s2);
+    }else if(s2.length-s1.length === 1 ){
+        return addOrDel1(s2, s1);
+    }else if(s2.length === s1.length){
+        return swap1(s1, s2);
+    }
+    return false;
+}
+
+console.log(oneAway("pale", "ple"));// true
+console.log(oneAway("pales", "pale"));// true
+console.log(oneAway("pale", "bale"));// true 
+console.log(oneAway("pale", "bake"));// false
