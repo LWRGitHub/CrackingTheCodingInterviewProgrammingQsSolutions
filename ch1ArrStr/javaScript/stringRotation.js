5f03381ef52803a49b56babbc21973ce0642c8b9
LWRGitHub <LoganReynoldsLWR@gmail.com>
2022-08-17 16:12:06 -0700

    strRotation + 0Matrix JS

--- ch1ArrStr/javaScript/stringRotation.js
+++ ch1ArrStr/javaScript/stringRotation.js
@@ -4,3 +4,21 @@
 // Gayel Laakmann McDowell(Feb 4, 2021) Cracking the Coding Interview; p. 91; 1.9 - String Rotation
 
 
+
+const isSubstring = (s1, s2, s1H={}, s2H={})=>{
+    if(s1.length !== s2.length) return false;
+    for(let i=0; i<s1.length; i++){
+        s1[i] in s1H ? s1H[s1[i]]++ : s1H[s1[i]] = 1;
+    }
+    for(let i=0; i<s2.length; i++){
+        s2[i] in s2H ? s2H[s2[i]]++ : s2H[s2[i]] = 1;
+    }
+    for(let key in s1H){
+        if(!(key in s2H) || s2H[key] !== s1H[key]) return false;
+    }
+    return true;
+}
+
+
+console.log(isSubstring("waterbottle", "erbottlewat"))// true
+console.log(isSubstring("waterbottle", "erbottlewatt"))// false
--- ch1ArrStr/javaScript/zeroMatrix.js
+++ ch1ArrStr/javaScript/zeroMatrix.js
@@ -3,3 +3,33 @@
 
 // Gayel Laakmann McDowell(Feb 4, 2021) Cracking the Coding Interview; p. 91; 1.8 - Zero Matrix
 
+
+
+const zeroMatrix = (matrix, ans=[]) =>{
+    for(let i=0; i<matrix.length; i++){
+        ans[i] = [];
+        for(let j=0; j<matrix.length; j++){
+            ans[i][j] = matrix[i][j];
+        }
+    }
+    for(let i=0; i<matrix.length; i++){
+        for(let j=0; j<matrix[i].length; j++){
+            console.log(`count ${i}: `,matrix)
+            if(matrix[i][j] === 0){
+                ans[i] = ans[i].map(x => 0);
+                col = i + 1;
+                while(col !== i){
+                    if(col===matrix.length){
+                        col = 0
+                        continue;
+                    }
+                    ans[col][j] = 0;
+                    col++;
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+console.log(zeroMatrix([[1,2,3],[4,0,6],[7,8,9]]))// [[1,0,3],[0,0,0],[7,0,9]] 
